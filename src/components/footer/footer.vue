5ea9a49a9b29af95227224f64b0ccb21944a173e
wangyanan <>
2017-11-15 18:27:19 +0800

    体验卡页面

--- src/components/footer/footer.vue
+++ src/components/footer/footer.vue
@@ -2,18 +2,17 @@
 	<div class="footer">
 		<ul>
 			<li>
-				<router-link to = '/'>
-				    <!-- <div  class="img" :style = "{background: $route.path === '/' ? '/img/icon_home_press@2x.png' : ''}"></div> -->
-					<p :style="{color: $route.path === '/' ? 'red' : ''}">我的</p>
-				</router-link>
-				
-			</li>
-			<li>
 				<router-link to = '/try'>
 				    <!-- <div  class="img" :style = "{background: $route.path === '/' ? '/img/icon_home_press@2x.png' : ''}"></div> -->
 					<p :style="{color: $route.path === '/try' ? 'red' : ''}">体验卡</p>
 				</router-link>
 				
+			</li>
+			<li>
+				<router-link to = '/'>
+				    <!-- <div  class="img" :style = "{background: $route.path === '/' ? '/img/icon_home_press@2x.png' : ''}"></div> -->
+					<p :style="{color: $route.path === '/' ? 'red' : ''}">我的</p>
+				</router-link>
 			</li>
 		</ul>
 	</div>
@@ -26,14 +25,18 @@ export default {
 <style scoped>
 .footer {
   position: fixed;
+  width:100%;
   bottom: 0rem;
   font-size: 0.2rem;
-  background: #fff;
-  height: 0.6rem;
-  line-height: 0.6rem;
+  background: #f7f7fa;
+  height: 0.8rem;
+  line-height: 0.8rem;
+  border-top:0.02rem solid #dcdbe0;
+  z-index: 100;
 }
 .footer ul {
   display: flex;
+  border-top:0.02rem solid #dcdbe0;
 }
 .footer li {
   margin-left: 1.5rem;
---
+++ src/components/place/place.vue
@@ -0,0 +1,3 @@
+<template>
+	<div>寻找位置</div>
+</template>
